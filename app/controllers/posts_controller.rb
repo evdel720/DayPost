2c88289d3e71cea177d145d2e9112fcde3deda21
Jo Jangmi <wkdal720@gmail.com>
2016-05-11 16:29:03 -0700

    created other actions and views for post and trying to make button for count

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,55 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post has been created."
+      redirect_to posts_path(@post)
+    else
+      flash.now[:alert] = "Post has not been created."
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Post has been updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Post has not been updated."
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    flash[:notice] = "Post has been deleted."
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
